4f4cb549a03ed498cc52fbcd043537a87701095c
EaRu723 <y.andrearusso@gmail.com>
2025-01-07 20:17:12 -0500

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Briefly Buzz</h1>
+      <h2>Your Daily Brief</h2>
+      <div className="buttons">
+        <button>Summarize</button>
+        <button>Rephrase</button>
+        <button>Expand</button>
+        <button>Simplify</button>
+      </div>
+    </div>
+  );
+}
+
+export default App; 
